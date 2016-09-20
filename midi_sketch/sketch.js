a11233fc9ef7ee021983e50919c21272b9e2fc11
Zoe Bachman <zoe.bachman@gmail.com>
2016-09-19 21:55:11 -0400

    first version of my p5js sketch

---
+++ midi_sketch/sketch.js
@@ -0,0 +1,50 @@
+//take a list of ip addresses
+//map ip addresses to frequencies
+//convert into tones
+//play tones
+
+
+//var notes1 = [60, 64, 67, 72];
+var notes1 = [194, 132, 197, 147];
+
+// var value = 25;
+//     var m = map(value, 0, 100, 0, width);
+//     ellipse(m, 50, 10, 10);
+
+var mappedValues;
+
+
+function setup() {
+  
+  
+  //remap the values
+  for (var i = 0; i < notes1.length; i++) {
+    
+  }
+  
+  m = map(notes1[0], 0, 256, 21, 108);
+  var i = 0;
+
+  osc = new p5.Oscillator('Triangle');
+  osc.start();
+  frameRate(1);
+  console.log(m)
+}
+
+// function remap(){
+//   for (var i = 0; i < notes1; i++){
+//     var m = map(notes1[i], 0, 255, 21, 108)
+//   }
+// }
+
+// function map(x, in_min, in_max, out_min, out_max) {
+//   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+// }
+
+
+
+function draw() {
+  //var freq = midiToFreq(m[i]);
+  //osc.freq(freq);
+
+}
